c1d84838e6017f9afd0e5947898e0f4398116925
Krisztina Nagy <krisztinahont@hotmail.com>
2020-03-17 09:37:02 +0100

    scrapes the page it should and gives back some message

--- risingstack_backend/app/calc.js
+++ risingstack_backend/app/calc.js
@@ -1,10 +1,18 @@
+const request_promise = require('request-promise');
+const cheerio = require("cheerio");
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+
+async function findNum (num) {
+  let solution = num.value;
+  console.log(solution);
+  return solution;
+}
+
+module.exports = {
+  findNum
+}
 
 
-function sum (arr) {
-    return arr.reduce(function(a, b) { 
-      return a + b
-    }, 0)
-  }
-  
-  module.exports.sum = sum;
 
--- risingstack_backend/app/index.js
+++ risingstack_backend/app/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
-//const calc = require('./calc');
+const service = require('./calc');
+//const app = express();
+
+/*app.get('/:ticker', (req, res) => {
+  let tickerSymbol = req.params.ticker;
+
+  service.scrapeFeedburner(tickerSymbol)
+  .then(list => { return service.getArticleData(list)
+  })
+  res.end();
+})*/
+
 const cheerio = require("cheerio");
+//const axios = require("axios");
 const request = require("request");
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", function(req, res) {
-  let url = "https://blog.risingstack.com/";
-  const getLinks = request(url, function(error, response, html) {
+
+router.post("/", async function(req, res) {
+  let num = req.body.body;
+  await service.findNum(num);
+  res.sendStatus(200);
+});
+
+router.get("/:page", async function(req, res) {
+  let page = req.params.page;
+  let url = `https://blog.risingstack.com/page/${page}`;
+  let getLinks = request(url, function(error, response, html) {
     if (!error) {
       $ = cheerio.load(html);
       let links = $(".post-title a");
@@ -15,12 +35,27 @@ router.get("/", function(req, res) {
         let href = $(link).attr("href");
         console.log($(link).text() + ": " + url + href);
       });
-      //res.send({"message":"hi"});
     } else {
       console.log(error);
     }
   });
-  res.send({"message":"hi"});
+  res.send({ "message" : "hello" });
 });
 
 module.exports = router;
+//let url = "https://blog.risingstack.com/"
+
+/*const getWebsiteContent = async (url) => {
+  try {
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
+
+  } catch (error) {
+
+    console.error(error)
+  }
+}
+
+getWebsiteContent(url)*/
+
+//module.exports = app;
