f44814919a027430a67b0df86a7c3b9f8e1c7ed6
Krisztina Nagy <krisztinahont@hotmail.com>
2020-03-18 15:41:33 +0100

    working for all the pages

--- risingstack_backend/app/calc.js
+++ risingstack_backend/app/calc.js
@@ -1,66 +1,66 @@
-const request = require('request-promise');
+const request = require("request-promise");
 const cheerio = require("cheerio");
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
-async function findNum (num) {
+async function findNum(num) {
   let solution = num.value;
   return solution;
 }
 
 async function parsePage(page) {
+  let data = [];
   try {
-    console.log(page);
-
-    for(let i =1; i <= page; i++) {
+    for (let i = 1; i <= page; i++) {
       base = `https://blog.risingstack.com/page/${i}`;
+      console.log(base);
       let mainHtml = await request(base);
       $ = cheerio.load(mainHtml);
 
-    const links = $(".post-title a");
-    const urlFinder = $(links).map(function(i, link) {
-      let href = $(link).attr("href");
-      let newUrl = `https://blog.risingstack.com${href}`;
-      return {
-        url: newUrl
-      }
-    }).get();
+      const links = $(".post-title a");
+      const urlFinder = $(links)
+        .map(function(i, link) {
+          let href = $(link).attr("href");
+          let newUrl = `https://blog.risingstack.com${href}`;
+          return {
+            url: newUrl
+          };
+        })
+        .get();
 
-    const data = await Promise.all(urlFinder.map(async (article) => {
-      try {
-        const articleHtml = await request(article.url);
-        $ = cheerio.load(articleHtml);
-        
-                const titles = $("article");
-                const headers = $(titles)
-                  .find("h1, h2, h3, h4")
-                  .map(function(i, title) {
-                    return $(title).text();
-                  }).get();
+      let temp = await Promise.all(
+        urlFinder.map(async article => {
+          try {
+            const articleHtml = await request(article.url);
+            $ = cheerio.load(articleHtml);
 
-                  return {
-                    headers
-                  }
+            const titles = $("article");
+            const headers = $(titles)
+              .find("h1, h2, h3, h4")
+              .map(function(i, title) {
+                return $(title).text();
+              })
+              .get();
 
-      } catch (error) {
-        return error.message;
-      }
-    }));
-    
+            return {
+              headers
+            };
+          } catch (error) {
+            return error.message;
+          }
+        })
+      );
+      data.push(...temp);
+    }
+    console.log(data);
     return data;
-  }
-  } catch (error){
+  } catch (error) {
     console.log("error: " + error);
   }
-
 }
-
 
 module.exports = {
   findNum,
   parsePage
-}
-
-
-
+};
