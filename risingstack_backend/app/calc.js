201fd2faa37e7d5588e5bd96849dbdf14c3ae712
Krisztina Nagy <krisztinahont@hotmail.com>
2020-03-18 14:58:56 +0100

    almost everything is working

--- risingstack_backend/app/calc.js
+++ risingstack_backend/app/calc.js
@@ -1,4 +1,4 @@
-const request_promise = require('request-promise');
+const request = require('request-promise');
 const cheerio = require("cheerio");
 const express = require("express");
 const router = express.Router();
@@ -6,12 +6,60 @@ router.use(express.json());
 
 async function findNum (num) {
   let solution = num.value;
-  console.log(solution);
   return solution;
 }
 
+async function parsePage(page) {
+  try {
+    console.log(page);
+
+    for(let i =1; i <= page; i++) {
+      base = `https://blog.risingstack.com/page/${i}`;
+      let mainHtml = await request(base);
+      $ = cheerio.load(mainHtml);
+
+    const links = $(".post-title a");
+    const urlFinder = $(links).map(function(i, link) {
+      let href = $(link).attr("href");
+      let newUrl = `https://blog.risingstack.com${href}`;
+      return {
+        url: newUrl
+      }
+    }).get();
+
+    const data = await Promise.all(urlFinder.map(async (article) => {
+      try {
+        const articleHtml = await request(article.url);
+        $ = cheerio.load(articleHtml);
+        
+                const titles = $("article");
+                const headers = $(titles)
+                  .find("h1, h2, h3, h4")
+                  .map(function(i, title) {
+                    return $(title).text();
+                  }).get();
+
+                  return {
+                    headers
+                  }
+
+      } catch (error) {
+        return error.message;
+      }
+    }));
+    
+    return data;
+  }
+  } catch (error){
+    console.log("error: " + error);
+  }
+
+}
+
+
 module.exports = {
-  findNum
+  findNum,
+  parsePage
 }
 
 
--- risingstack_backend/app/index.js
+++ risingstack_backend/app/index.js
@@ -1,19 +1,5 @@
 const express = require("express");
-const service = require('./calc');
-//const app = express();
-
-/*app.get('/:ticker', (req, res) => {
-  let tickerSymbol = req.params.ticker;
-
-  service.scrapeFeedburner(tickerSymbol)
-  .then(list => { return service.getArticleData(list)
-  })
-  res.end();
-})*/
-
-const cheerio = require("cheerio");
-//const axios = require("axios");
-const request = require("request");
+const service = require("./calc");
 const router = express.Router();
 router.use(express.json());
 
@@ -26,36 +12,9 @@ router.post("/", async function(req, res) {
 
 router.get("/:page", async function(req, res) {
   let page = req.params.page;
-  let url = `https://blog.risingstack.com/page/${page}`;
-  let getLinks = request(url, function(error, response, html) {
-    if (!error) {
-      $ = cheerio.load(html);
-      let links = $(".post-title a");
-      $(links).each(function(i, link) {
-        let href = $(link).attr("href");
-        console.log($(link).text() + ": " + url + href);
-      });
-    } else {
-      console.log(error);
-    }
-  });
-  res.send({ "message" : "hello" });
+  const base= page;
+  let data = await service.parsePage(base);
+  res.send({message: data});
 });
 
 module.exports = router;
-//let url = "https://blog.risingstack.com/"
-
-/*const getWebsiteContent = async (url) => {
-  try {
-    const response = await axios.get(url)
-    const $ = cheerio.load(response.data)
-
-  } catch (error) {
-
-    console.error(error)
-  }
-}
-
-getWebsiteContent(url)*/
-
-//module.exports = app;
